7bab35eccc0b43e76006db2d14c1475850618045
carolinecarlson <cjc39@students.calvin.edu>
2020-02-22 16:54:09 -0500

    Color change and other updates

--- dinefind/src/components/Preferences/index.js
+++ dinefind/src/components/Preferences/index.js
@@ -6,14 +6,15 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Rating from '@material-ui/lab/Rating';
 import {FormGroup} from "@material-ui/core";
 import Box from "@material-ui/core/Box";
-import {Restaurant} from "../Restaurant/Restaurant";
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import RadioButtonsGroup from "./RadioButton";
 
 const marks = [
     {
         value: 0,
-    },
-    {
-        value: 5,
     },
     {
         value: 5,
@@ -41,18 +42,19 @@ function valuetext(value) {
 function valueLabelFormat(value) {
     return marks.findIndex(mark => mark.value === value) + 1;
 }
+
 export default function Preferences() {
         const [value, setValue] = React.useState(2);
-
         const [state, setState] = React.useState({
             open: true,
             chains: true,
             visited: true,
             distance: 0,
-            rating: 0
+            rating: 0,
     });
     const handleChange = name => event => {
         setState({ ...state, [name]: event.target.checked });
+        setValue(event.target.value);
     };
     return(
         <div className="centered-container row">
@@ -61,42 +63,49 @@ export default function Preferences() {
                 <h1>Are you hungry...</h1>
                 <h3 className = "white">to try something new ?</h3>
                 <p>Find a restaurant that you haven't tried yet! Our default settings help you find close, local, and new restaurants. Before we begin, check your search preferences to see if you want to make any changes. You can always come back to make changes at any time.</p>
-                <h4 className="white">My Preferences</h4>
-                <Typography id="discrete-slider-always" gutterBottom>
-                    Distance in miles:
-                </Typography>
-                <Slider
-                    name="distance"
-                    className="slider"
-                    defaultValue={10}
-                    getAriaValueText={valuetext}
-                    valueLabelDisplay="auto"
-                    step={5}
-                    marks={marks} />
-                <FormControlLabel control={
-                    <Switch className="switch" checked={state.chains} onChange={handleChange('chains')} value="chains" />
-                } label="Show chain restaurants"
+                <div className="row">
+                <div className="col-6 preferences">
+                    <h4 className="white">My Preferences</h4>
+                    <Typography id="discrete-slider-always" gutterBottom>
+                        Distance in miles:
+                    </Typography>
+                    <Slider
+                        name="distance"
+                        className="slider"
+                        defaultValue={10}
+                        getAriaValueText={valuetext}
+                        valueLabelDisplay="auto"
+                        step={5}
+                        marks={marks} />
+                    <FormControlLabel control={
+                        <Switch className="switch" checked={state.chains} onChange={handleChange('chains')} value="chains" />
+                    } label="Show chain restaurants"
+                        />
+                    <FormControlLabel control={
+                    <Switch className="switch" checked={state.visited} onChange={handleChange('visited')} value="visited" />
+                    } label="Show restaurants I have dined at before"
+                         />
+                    <div className="indent"><p>(You will have the option to select and keep track of the restaurants you have been to on the search results page)</p></div>
+                    <FormControlLabel control={
+                        <Switch className="switch" checked={state.open} onChange={handleChange('open')} value="open" />
+                    } label="Show open & closed restaurants"
                     />
-                <FormControlLabel control={
-                <Switch className="switch" checked={state.visited} onChange={handleChange('visited')} value="visited" />
-                } label="Show restaurants I have dined at before"
-                     />
-                <div class="indent"><p>(You will have the option to select and keep track of the restaurants you have been to on the search results page)</p></div>
-                <FormControlLabel control={
-                    <Switch className="switch" checked={state.open} onChange={handleChange('open')} value="open" />
-                } label="Show open & closed restaurants"
-                />
-                <Box className="fixPadding" component="fieldset" mb={3} borderColor="transparent">
-                <Typography component="legend">How should it be rated?</Typography>
-                <Rating
-                    name="rating"
-                    value={value}
-                    onChange={(event, newValue) => {
-                    setValue(newValue);
-                    }}
-                    />
-                </Box>
-                <h4 className="white">My Filters</h4>
+                    <Box className="fixPadding" component="fieldset" mb={3} borderColor="transparent">
+                    <Typography component="legend">How should it be rated?</Typography>
+                    <Rating
+                        name="rating"
+                        value={value}
+                        onChange={(event, newValue) => {
+                        setValue(newValue);
+                        }}
+                        />
+                    </Box>
+                </div>
+                <div className="col-6 filters">
+                    <h4 className="white">My Filters</h4>
+                    <RadioButtonsGroup />
+                </div>
+                </div>
                 <button className="signInField signInButton"
                     type = "submit"
                     onClick={event => {
